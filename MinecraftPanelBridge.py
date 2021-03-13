c90ef8a0979c0e7ce6959106efc366cbe1d5938f
anthonymesa <admesa@pm.me>
2021-03-12 23:58:54 -0500

    Update MinecraftPanelBridge.py

--- MinecraftPanelBridge.py
+++ MinecraftPanelBridge.py
@@ -1,10 +1,48 @@
-import discord
+# MinecraftPanelBridge
+# Author: Anthony Mesa
+#================================
 
+import discord
+import json
+
+# Get discord client
 client = discord.Client()
+
+# Get MinecraftPanelBridge sconfiguration data
+with open('path') as config_file:
+    config_data = json.load(config_file)
+
+print(data)
+
+# Define available commands
+#================================
+
+# Test command
+
+@bot.command()
+async def test(ctx):
+    ctx.send('test')
+    pass
+
+bot.add_command(test)
+
+#----------------------
+
+# second test command
+
+@bot.command()
+async def test2(ctx):
+    ctx.send('test2')
+    pass
+
+bot.add_command(test2)
+
+# Define discord client events
+#================================
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Bot online: {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
@@ -13,5 +51,8 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+# Run discord bot
+#================================
 
 client.run('NzMyNjcxMjAzMTUxNzA4MTgw.Xw3_Ow.BfTd-NAKmM-RlwkDM2SA2VrtZIg')
